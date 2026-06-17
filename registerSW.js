58c7e7ff53b755467efe3fd51bbf72dbd789ff2b
eitanisrael <eitanisrael461@gmail.com>
2026-06-17 21:54:22 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Tweetelligent/dist/sw.js', { scope: '/Tweetelligent/dist/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Tweetelligent/sw.js', { scope: '/Tweetelligent/' })})}
